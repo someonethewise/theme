1a5589081661f550840e9e7083e1be1325e2e7fd
Andrew Munro <andrew@sumobi.com>
2014-04-01 23:32:29 +1300

    Various page updates

--- functions.php
+++ functions.php
@@ -14,24 +14,141 @@ require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'scripts.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'sharing.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'gforms.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'ajax-functions.php' );
+require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'navigation.php' );
+require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'template-tags.php' );
+require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'post-types.php' );
+
+if ( ! function_exists( 'affwp_setup' ) ) :
+/**
+ * AffiliateWP setup.
+ *
+ * Set up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support post thumbnails.
+ *
+ * @since AffiliateWP 1.0
+ */
+function affwp_setup() {
+
+	load_theme_textdomain( 'affwp', get_template_directory() . '/languages' );
+
+	// Add RSS feed links to <head> for posts and comments.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Enable support for Post Thumbnails, and declare two sizes.
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 672, 372, true );
+	add_image_size( 'affwp-full-width', 1038, 576, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary'   => __( 'Primary menu', 'affwp' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list',
+	) );
+
+	// This theme uses its own gallery styles.
+	add_filter( 'use_default_gallery_style', '__return_false' );
+}
+endif; // affwp_setup
+add_action( 'after_setup_theme', 'affwp_setup' );
+
 
 /**
- * Typekit fonts
- * @return [type] [description]
- */
-function affwp_webfonts() { ?>
+ * Fittext
+ *
+ * @since 1.0
+*/
+function affwp_fittext() { 
+?>
 	<script type="text/javascript">
-  (function(d) {
-    var config = {
-      kitId: 'llz0jhz',
-      scriptTimeout: 3000
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
-<?php }
-add_action( 'wp_head', 'affwp_webfonts', 5 );
+	 jQuery(document).ready(function($) {
+	 	jQuery("h1.intro").fitText( 2.1, { maxFontSize: '56px' });	
 
+	 });
+	</script>
+<?php }
+add_action( 'wp_footer', 'affwp_fittext', 50 );
+
+/**
+ * Filter menu and add 'has-sub-menu' class to parent
+ *
+ * @since 1.0
+*/
+function affwp_add_has_sub_menu_parent_class( $items ) {
+  
+  $parents = array();
+  foreach ( $items as $item ) {
+    if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+      $parents[] = $item->menu_item_parent;
+    }
+  }
+  
+  foreach ( $items as $item ) {
+    if ( in_array( $item->ID, $parents ) ) {
+      $item->classes[] = 'has-sub-menu'; 
+    }
+  }
+  
+  return $items;    
+}
+add_filter( 'wp_nav_menu_objects', 'affwp_add_has_sub_menu_parent_class' );
+
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @since 1.0
+ */
+function affwp_body_classes( $classes ) {
+	global $post;
+
+	// Adds a class of 'has-featured-image' if the current post has a featured image
+	if ( isset( $post->ID ) && get_the_post_thumbnail( $post->ID ) )
+		$classes[] = 'has-featured-image';
+
+	// Adds a class of group-blog to blogs with more than 1 published author
+	if ( is_multi_author() )
+		$classes[] = 'group-blog';
+
+	// Adds a class of 'blog' if the blog template is being used. The homepage when used as a blog also has this class already
+	if ( is_page_template( 'page-templates/blog.php' ) )
+		$classes[] = 'blog';
+
+	if ( is_page_template( 'page-templates/full-width.php' ) )
+		$classes[] = 'full-width';
+	
+	return $classes;
+}
+add_filter( 'body_class', 'affwp_body_classes' );
+
+/**
+ * Register three AffiliateWP widget areas.
+ *
+ * @since 1.0
+ *
+ * @return void
+ */
+function affwp_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Primary Sidebar', 'affwp' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Main sidebar', 'affwp' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'affwp_widgets_init' );
 
 /**
  * Create a nicely formatted and more specific title element text for output
---
+++ page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @since AffiliateWP 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+
+
+	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
+
+<?php
+//get_sidebar();
+get_footer();
