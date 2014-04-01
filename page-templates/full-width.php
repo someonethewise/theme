2e84210d770649c8937201a485e13cb360e18237
Andrew Munro <andrew@sumobi.com>
2014-04-01 23:32:14 +1300

    Full width page template

---
+++ page-templates/full-width.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+		endwhile;
+	?>
+	</div><!-- #primary -->
+
+<?php
+get_footer();
