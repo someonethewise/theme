926c969313fed30407b3c5254d6433c0a8aac83d
Andrew Munro <andrew@sumobi.com>
2014-04-10 20:42:24 +1200

    Fix excerpt showing on single doc page

--- single-docs.php
+++ single-docs.php
@@ -1,12 +1,7 @@
 <?php
 /**
  * The Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
  */
-
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -14,7 +9,7 @@ get_header(); ?>
 	<?php
 
 	?>
-	<a class="back" href="/support/documentation">Back to documentation</a>
+	<a class="back" href="<?php echo site_url( 'support/documentation' ); ?>">Back to documentation</a>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
@@ -22,11 +17,7 @@ get_header(); ?>
 				<header class="entry-header">
 					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 				</header>
-	<?php 
-		 		affwp_post_thumbnail();
-		 		
-		 		the_excerpt();
-		 	?>
+			<?php affwp_post_thumbnail(); ?>
 				<div class="entry-content">
 					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'affwp' ) ); ?>
 				</div>
