2b86d659827e0165f5ed19c06c73aee0b3776478
Andrew Munro <andrew@sumobi.com>
2014-04-29 00:15:31 +1200

    footer update

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@
 				</div>
 
 				<div class="item">
-				<h3>Links</h3>
+				<h3>Pages</h3>
 					<ul class="page-links">
 						<li><a href="<?php echo site_url( 'pricing' ); ?>" title="Pricing">Pricing</a></li>
 						<li><a href="<?php echo site_url( 'about' ); ?>" title="About">About</a></li>
