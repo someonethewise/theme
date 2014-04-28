ccfac06a5a9d7c1d02bedacbe682980881c7e8e4
Andrew Munro <andrew@sumobi.com>
2014-04-28 21:16:16 +1200

    sharing JS improvements

--- includes/sharing.php
+++ includes/sharing.php
@@ -79,59 +79,59 @@ function affwp_social_scripts() {
 	
 	?>
 	<script type="text/javascript">
+		jQuery(document).ready(function() {
 
-	<?php 
-	/**
-	 * Twitter
-	*/
-	?>
-  	window.twttr = (function (d,s,id) {
-	  var t, js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-	  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-	  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
-	}(document, "script", "twitter-wjs"));
+			<?php 
+			/**
+			 * Twitter
+			*/
+			?>
+		  	window.twttr = (function (d,s,id) {
+			  var t, js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+			  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+			  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+			}(document, "script", "twitter-wjs"));
 
-	<?php 
-	/**
-	 * Google +
-	*/
-	?>
-	window.___gcfg = {
-	  lang: 'en-US',
-	  parsetags: 'onload'
-	};
+			<?php 
+			/**
+			 * Google +
+			*/
+			?>
+			window.___gcfg = {
+			  lang: 'en-US',
+			  parsetags: 'onload'
+			};
 
-	(function() {
-	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	    po.src = 'https://apis.google.com/js/plusone.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
+			(function() {
+			    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			    po.src = 'https://apis.google.com/js/plusone.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+			  })();
 
-	<?php
-	/**
-	 * Facebook
-	*/
-	?>
+			<?php
+			/**
+			 * Facebook
+			*/
+			?>
+			(function(d, s, id) {
+			     var js, fjs = d.getElementsByTagName(s)[0];
+			     if (d.getElementById(id)) {return;}
+			     js = d.createElement(s); js.id = id;
+			     js.src = "//connect.facebook.net/en_US/all.js";
+			     fjs.parentNode.insertBefore(js, fjs);
+			 }(document, 'script', 'facebook-jssdk'));
 
-	(function(d, s, id) {
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/all.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	 }(document, 'script', 'facebook-jssdk'));
+			window.fbAsyncInit = function() {
+			    // init the FB JS SDK
+			    FB.init({
+			      status	: true,
+			      cookie	: true,                               
+			      xfbml		: true                              
+			    });
 
-	window.fbAsyncInit = function() {
-	    // init the FB JS SDK
-	    FB.init({
-	      status	: true,
-	      cookie	: true,                               
-	      xfbml		: true                              
-	    });
-
-	};
-
+			};
+		});	
 	</script>
 	<?php
 }
