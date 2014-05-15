6514698c1c3f61fa1ed90afddad6542be59fb641
Andrew Munro <andrew@sumobi.com>
2014-05-15 16:21:11 +1200

    fix gists

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if ( ! defined( 'EDD_SLUG' ) )
 	define( 'EDD_SLUG', 'addons' );
 
 if ( ! defined( 'AFFWP_THEME_VERSION' ) )
-	define( 'AFFWP_THEME_VERSION', '1.3.6' );
+	define( 'AFFWP_THEME_VERSION', '1.3.7' );
 
 if ( ! defined( 'AFFWP_INCLUDES_DIR' ) )
 	define( 'AFFWP_INCLUDES_DIR', trailingslashit( get_template_directory() ) . 'includes' ); /* Sets the path to the theme's includes directory. */
