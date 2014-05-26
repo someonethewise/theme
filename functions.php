d0c40da301f2756bc07a4687e51269e83850b819
Andrew Munro <andrew@sumobi.com>
2014-05-26 17:57:20 +1200

    affiliate banner updates

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if ( ! defined( 'EDD_SLUG' ) )
 	define( 'EDD_SLUG', 'addons' );
 
 if ( ! defined( 'AFFWP_THEME_VERSION' ) )
-	define( 'AFFWP_THEME_VERSION', '1.4.1' );
+	define( 'AFFWP_THEME_VERSION', '1.4.2' );
 
 if ( ! defined( 'AFFWP_INCLUDES_DIR' ) )
 	define( 'AFFWP_INCLUDES_DIR', trailingslashit( get_template_directory() ) . 'includes' ); /* Sets the path to the theme's includes directory. */
@@ -29,7 +29,6 @@ require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'checkout.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'edd.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'metaboxes.php' );
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'custom-filters.php' );
-
 require_once( trailingslashit( AFFWP_INCLUDES_DIR ) . 'blog.php' );
 
 
