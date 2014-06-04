9b7c67cb4501c81ad62f410ce5ccb15ea4135e71
Andrew Munro <andrew@sumobi.com>
2014-06-04 14:48:57 +1200

    button text update

--- header.php
+++ header.php
@@ -68,9 +68,9 @@
 				</h1>
 
 				<div id="mascot"></div>
-				<?php echo affwp_button( 'Get Started', site_url( 'pricing' ) ); ?>
+				<?php echo affwp_button( 'Get Started Now', site_url( 'pricing' ) ); ?>
 				<span>or</span>
-				<a class="button large" href="http://demo.affiliatewp.com" target="_blank">View Demo</a>
+				<a class="button large" href="http://demo.affiliatewp.com" target="_blank">Try The Demo</a>
 			</div>		
 		<?php endif; ?>
 
