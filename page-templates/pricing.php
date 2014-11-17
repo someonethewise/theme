6a97f410aadf2cbd0451f72770685178a7e3f245
Andrew Munro <andrew@sumobi.com>
2014-11-17 18:43:09 +1300

    Pricing page copy improvements

--- page-templates/pricing.php
+++ page-templates/pricing.php
@@ -59,18 +59,20 @@ $third_party_add_ons   = affwp_get_add_on_count( '3rd-party' );
 			
 			<article>
 				<h4>Do I need to renew my license?</h4>
-				<p>The license key is valid for one year from the purchase date. An active license key is needed for access to automatic updates and support. Your license can be renewed each year at a 40% discount of the current price.</p>
+				<p>Your license key is valid for one year from the purchase date. You need an active license key for continued access to automatic updates and support. If you renew your license each year you’ll receive a huge 40% discount off the current price!</p>
 			</article>
 			
 			<article>
 				<h4>Can I upgrade my license?</h4>
-				<p>Yes, we offer a 1-click upgrade process from your <a href="<?php echo site_url( 'account' ); ?>" title="Account" >account</a> page.</p>
+				<p>Yes, you can easily upgrade your license from your <a href="<?php echo site_url( 'account' ); ?>" title="Account" >account</a> page.</p>
 			</article>
 
 			<article>
 				<h4>Do I get access to add-ons?</h4>
-				<p>There are <a href="#modal-offical-free-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $official_free_add_ons; ?> official free add-ons</a> and <a href="#modal-third-party-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $third_party_add_ons; ?> third party add-ons</a> (free and paid) available for all license holders.</p>
-				<p>Those lucky enough to have either an ultimate or professional license receive free access to <a href="#modal-pro-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $professional_add_ons; ?> pro add-ons</a> as a special perk, including any we release in the future.</p>
+				<p>We have <a href="#modal-offical-free-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $official_free_add_ons; ?> official free add-ons</a> and <a href="#modal-third-party-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $third_party_add_ons; ?> third party add-ons</a> (free and paid) available for all license holders.</p>
+
+				<p>If you have either an ultimate or professional license you also receive free access to <a href="#modal-pro-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $professional_add_ons; ?> pro add-ons</a>, plus any additional add-ons we release in the future.</p>
+
 			</article>
 
 			<article>
@@ -84,33 +86,22 @@ $third_party_add_ons   = affwp_get_add_on_count( '3rd-party' );
 
 			<article>
 				<h4>Do you have a refund policy?</h4>
-				<p><a href="#refund-policy" class="popup-content" data-effect="mfp-move-from-bottom">Yes we do</a>! We firmly believe in and stand behind the quality of our product and will refund 100% of your money if you are unhappy with the plugin.</p>
+				<p><a href="#refund-policy" class="popup-content" data-effect="mfp-move-from-bottom">Yes we do</a>! We stand behind the quality of our product and will refund 100% of your money if you are unhappy with the plugin.</p>
 			</article>
 
-			<?php /*
-			<article>
-				<h4>What are the benefits of the developer license?</h4>
-				<p>In addition to being able to use AffiliateWP on an unlimited number of sites, a developer license also gives you access to all <a href="#modal-pro-add-ons" class="popup-content" data-effect="mfp-move-from-bottom"><?php echo $professional_add_ons; ?> pro add-ons</a>, including any we release in the future. This makes it a considerably valuable license to hold.</p>
-			</article>
-			*/ ?>
-		
 			<article>
 				<h4>Do I get updates for the plugin?</h4>
-				<p>Yes! Automatic updates are delivered 100% free of charge to all users with a valid license key.</p>
+				<p>Yes! Automatic updates are available free of charge to all users with a valid license key.</p>
 			</article>
 
 			<article>
 				<h4>Do you offer support if I need help?</h4>
-				<p>Yes! We believe that top-notch support is key for a quality product and will do our very best to resolve any issues you encounter via our <a title="Support" href="<?php echo site_url( 'support' ); ?>">support page</a>.</p>
+				<p>Yes! Top-notch customer support is key for a quality product, so we’ll do our very best to resolve any issues you encounter via our <a title="Support" href="<?php echo site_url( 'support' ); ?>">support page</a>.</p>
 			</article>
 
-		
-
-			
-			
 			<article>
 				<h4>I have other pre-sale questions, can you help?</h4>
-				<p>Yes! You are welcome to ask any question you wish from our <a title="Support" href="<?php echo site_url( 'support' ); ?>">support page</a>.</p>
+				<p>Yes! You can ask us any question through our <a title="Support" href="<?php echo site_url( 'support' ); ?>">support page</a>.</p>
 			</article>
 			
 		</div>
