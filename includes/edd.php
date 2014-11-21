a54c17b4e4bff2c35c14b48d23414a1e542568b5
Pippin Williamson <pippin@pippinsplugins.com>
2014-11-20 20:58:33 -0600

    Fix syntax error

--- includes/edd.php
+++ includes/edd.php
@@ -254,8 +254,8 @@ function affwp_cart_details_item_discount( $discount, $item ) {
 	}
 
 	$license_id = edd_software_licensing()->get_license_by_key( $key );
-	$price_id   = EDD()->session->get( 'upgrade_price_id' )
-	$discount   = EDD()->session->get( 'upgrade_discount' )
+	$price_id   = EDD()->session->get( 'upgrade_price_id' );
+	$discount   = EDD()->session->get( 'upgrade_discount' );
 
 	if( $discount ) {
 
