f4b471ed93cf75cb6ed27ecd60e147894853ae4d
Pippin Williamson <pippin@pippinsplugins.com>
2014-11-25 15:14:24 -0600

    Price ID is 3, not 4

--- edd_templates/licenses-manage-overview.php
+++ edd_templates/licenses-manage-overview.php
@@ -45,7 +45,7 @@ if ( $keys ) : ?>
 				</td>
 				<td class="edd_sl_license_status <?php echo edd_software_licensing()->get_license_status( $license->ID ); ?>"><?php echo edd_software_licensing()->license_status( $license->ID ); ?></td>
 				<td><span class="edd_sl_limit_used"><?php echo edd_software_licensing()->get_site_count( $license->ID ); ?></span><span class="edd_sl_limit_sep">&nbsp;/&nbsp;</span><span class="edd_sl_limit_max"><?php echo edd_software_licensing()->license_limit( $license->ID ); ?></span></td>
-				<td><?php if( 4 === (int) $price_id ) { echo 'never'; } else { echo date_i18n( 'F j, Y', edd_software_licensing()->get_license_expiration( $license->ID ) ); } ?></td>
+				<td><?php if( 3 === (int) $price_id ) { echo 'never'; } else { echo date_i18n( 'F j, Y', edd_software_licensing()->get_license_expiration( $license->ID ) ); } ?></td>
 				<?php if( ! edd_software_licensing()->force_increase() ) : ?>
 				<td><a href="<?php echo esc_url( add_query_arg( 'license_id', $license->ID ) ); ?>"><?php _e( 'Manage Sites', 'edd_sl' ); ?></a></td>
 				<?php endif; ?>
