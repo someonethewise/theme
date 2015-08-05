41c85a87326a79412364362326bce530976e2859
Andrew Munro <andrew@sumobi.com>
2015-08-05 14:08:21 +1200

    Show expire notice

--- includes/notices.php
+++ includes/notices.php
@@ -8,59 +8,76 @@ function affwp_dequeue_style() {
 }
 add_action( 'wp_enqueue_scripts', 'affwp_dequeue_style' );
 
-
-
+/**
+ * Show notices
+ */
 function affwp_display_notice() {
 
 	/// this displays the notification area if the user has not read it before
-	global $user_ID; 
-	$notice_args = array('post_type' => 'notices', 'posts_per_page' => 1, 'meta_key' => '_enabled', 'meta_value' => '1');
-	$notices = get_posts($notice_args);
-	if($notices) :
-		foreach ($notices as $notice) {
-		
-			$icon = strtolower(str_replace(' ', '_', get_post_meta($notice->ID, '_icon', true)));
+	global $user_ID;
+
+	$notice_args = array(
+		'post_type'      => 'notices',
+		'posts_per_page' => 1,
+		'meta_key'       => '_enabled',
+		'meta_value'     => '1'
+	);
+
+	$expired = get_page_by_title( 'License expired', 'OBJECT', 'notices' );
+
+	if ( $expired && ! affwp_has_license_expired() ) {
+		$notice_args['exclude'] = array( $expired->ID );
+	}
+
+	$notices = get_posts( $notice_args );
+
+	if ( $notices ) {
+
+		foreach ( $notices as $key => $notice ) {
+
 			$logged_in_only = get_post_meta($notice->ID, '_notice_for_logged_in_only', true);
-			
+
 			$can_view = false;
-			if( $logged_in_only == 'All' ) {
+
+			if ( $logged_in_only == 'All' ) {
 				$can_view = true;
-			} else if( $logged_in_only == 'Logged In' && is_user_logged_in() ) {
+			} else if ( $logged_in_only == 'Logged In' && is_user_logged_in() ) {
 				$can_view = true;
-			} else if( $logged_in_only == 'Logged Out' && !is_user_logged_in() ) {
+			} else if ( $logged_in_only == 'Logged Out' && ! is_user_logged_in() ) {
 				$can_view = true;
 			}
-			
-			if($can_view) {			
-				if(pippin_check_notice_is_read($notice->ID, $user_ID) != true) { ?>
-					<div id="notification-area" class="snp-hidden">
-			
-							<div class="notice-content">	
-								<?php
 
-								/*<h3><?php echo get_the_title($notice->ID); ?></h3>					
-									*/ ?>
+			if ( $can_view ) {
+
+				if ( pippin_check_notice_is_read( $notice->ID, $user_ID ) != true ) { ?>
+
+					<div id="notification-area" class="snp-hidden">
+
+							<div class="notice-content">
 
 								<svg id="announcement" width="32px" height="32px">
 								   <use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svg-defs.svg#icon-announcement'; ?>"></use>
 								</svg>
 
 								<?php echo do_shortcode(wpautop($notice->post_content)); ?>
+
 							</div>
 
 							<?php if(!get_post_meta($notice->ID, '_hide_close', true)) { ?>
+
 							<a class="remove-notice" href="#" id="remove-notice" rel="<?php echo $notice->ID; ?>">
 							<svg width="24px" height="24px">
 								   <use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svg-defs.svg#icon-remove'; ?>"></use>
 								</svg>
 							</a>
-							
+
 							<?php } ?>
 					</div>
-				<?php } 
-			}			
+				<?php }
+
+			} // can view
 		}
-	endif;
+	}
 }
 
 remove_action( 'wp_footer', 'pippin_display_notice' );
