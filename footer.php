ffe1561ae376a5af05efe3fc8816bb7cc5395ec3
Pippin Williamson <pippin@pippinsplugins.com>
2015-09-24 14:57:47 -0500

    Add HelpScout beacon for testing

--- footer.php
+++ footer.php
@@ -110,5 +110,6 @@
 </div> <!-- #site -->	
 
 <?php wp_footer(); ?>
+<script>!function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config={docs:{enabled:!0,baseUrl:"http://affiliatewp.helpscoutdocs.com/"},contact:{enabled:!1,formId:"5bba41f1-62f6-11e5-8846-0e599dc12a51"}};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});</script>
 </body>
 </html>
