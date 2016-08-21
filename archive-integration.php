978384771f7038da0095a7316b4c272d6838511e
Andrew Munro <andrew@sumobi.com>
2016-08-21 20:47:43 +1200

    integration tweaks

--- archive-integration.php
+++ archive-integration.php
@@ -7,14 +7,14 @@ get_header(); ?>
 <?php themedd_post_header( array( 'title' => post_type_archive_title( '', false ), 'subtitle' => 'AffiliateWP integrates seamlessly with popular eCommerce, Membership, Form, and Invoice WordPress plugins.' ) ); ?>
 
 <?php if ( have_posts() ) : ?>
-<section class="container-fluid highlight">
+<section class="container-fluid highlight pv-xs-2 pv-sm-3 pv-lg-4">
     <div class="wrapper wide container-fluid">
 		<div class="grid row has-overlay">
 			<?php while ( have_posts() ) : the_post();
 			global $post;
 			?>
 
-			<div <?php post_class( array( 'col-xs-12', 'col-sm-6', 'col-md-4', 'grid-item', $post->post_name ) ); ?>>
+			<div <?php post_class( array( 'col-xs-12', 'col-sm-6', 'col-md-4', 'grid-item', 'mb-xs-2', 'mb-sm-0', $post->post_name ) ); ?>>
 
 				<div class="grid-item-inner">
 
