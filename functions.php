2d42be5a8d922b920155d053ea39f9b73580affc
Andrew Munro <andrew@sumobi.com>
2016-08-22 19:59:25 +1200

    Integration CSS tweaks

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'AFFWP_THEME_INCLUDES_DIR' ) ) {
 }
 
 if ( ! defined( 'AFFWP_THEME_VERSION' ) ) {
-	define( 'AFFWP_THEME_VERSION', '1.0.3' );
+	define( 'AFFWP_THEME_VERSION', '1.0.4' );
 }
 
 /**
