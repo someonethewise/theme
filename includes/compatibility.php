856514d67ee2ebc8380f9f643c38bde386320728
Andrew Munro <andrew@sumobi.com>
2016-08-26 14:34:29 +1200

    Remove subtitles from download grid

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -2,3 +2,14 @@
 /**
  * Compatibility tweaks
  */
+
+function affwp_theme_filter_title( $title, $id ) {
+	global $post;
+
+	if ( has_shortcode( $post->post_content, 'downloads' ) ) {
+		add_filter( 'subtitle_view_supported', '__return_false' );
+	}
+
+	return $title;
+}
+add_filter( 'the_title', 'affwp_theme_filter_title', 10, 2 );
