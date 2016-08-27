7ee6dd3e0d147f3ea831994fe2bb7fccfccb581d
Andrew Munro <andrew@sumobi.com>
2016-08-27 16:18:46 +1200

    footer improvements

--- includes/footer.php
+++ includes/footer.php
@@ -18,51 +18,64 @@ function affwp_theme_footer_menu() {
 			<div class="col-xs-12 col-sm-12">
 				<div class="row">
 					<div class="col-xs-12 col-sm-3">
-						<div class="wrap">
-							<h4>Product</h4>
-							<ul>
-								<li><a href="<?php echo site_url( 'pricing' ); ?>">Pricing</a></li>
-								<li><a href="<?php echo site_url( 'features' ); ?>">All features</a></li>
-								<li><a href="<?php echo site_url( 'screenshots' ); ?>">Screenshots</a></li>
-								<li><a href="<?php echo site_url( 'add-ons' ); ?>">Add-ons</a></li>
-								<li><a href="<?php echo site_url( 'testimonials' ); ?>">Testimonials</a></li>
-								<li><a href="<?php echo get_stylesheet_directory_uri() . '/changelog.php'; ?>" id="affwp-changelog" data-effect="mfp-move-from-bottom">Changelog</a></li>
-							</ul>
-						</div>
+
+						<h4>Product</h4>
+						<ul>
+							<li><a href="<?php echo site_url( 'pricing' ); ?>">Pricing</a></li>
+							<li><a href="<?php echo site_url( 'features' ); ?>">All features</a></li>
+							<li><a href="<?php echo site_url( 'screenshots' ); ?>">Screenshots</a></li>
+							<li><a href="<?php echo site_url( 'add-ons' ); ?>">Add-ons</a></li>
+							<li><a href="<?php echo site_url( 'testimonials' ); ?>">Testimonials</a></li>
+							<li><a href="<?php echo get_stylesheet_directory_uri() . '/changelog.php'; ?>" id="affwp-changelog" data-effect="mfp-move-from-bottom">Changelog</a></li>
+						</ul>
+
 					</div>
 
 					<div class="col-xs-12 col-sm-3">
-						<div class="wrap">
-							<h4>Company</h4>
-							<ul>
-								<li><a href="<?php echo esc_url( site_url('about') ); ?>">About us</a></li>
-								<li><a href="<?php echo esc_url( site_url('blog') ); ?>">Blog</a></li>
-								<li><a href="<?php echo esc_url( site_url('brand-assets') ); ?>">Brand assets</a></li>
-								<li><a href="<?php echo esc_url( site_url('refund-policy') ); ?>">Refund policy</a></li>
-								<li><a href="https://twitter.com/affwp" target="_blank">Follow us on Twitter</a></li>
-							</ul>
-						</div>
+
+						<h4>Company</h4>
+						<ul>
+							<li><a href="<?php echo esc_url( site_url('about') ); ?>">About us</a></li>
+							<li><a href="<?php echo esc_url( site_url('blog') ); ?>">Blog</a></li>
+							<li><a href="<?php echo esc_url( site_url('brand-assets') ); ?>">Brand assets</a></li>
+							<li><a href="<?php echo esc_url( site_url('refund-policy') ); ?>">Refund policy</a></li>
+							<li><a href="https://twitter.com/affwp" target="_blank">Follow us on Twitter</a></li>
+						</ul>
+
 					</div>
 
 					<div class="col-xs-12 col-sm-3">
-						<div class="wrap">
-							<h4>Get Help</h4>
-							<ul>
-								<li><a href="<?php echo esc_url( site_url('support') ); ?>">Support</a></li>
-								<li><a href="http://docs.affiliatewp.com/" target="_blank">Documentation</a></li>
-								<li><a href="<?php echo esc_url( site_url('consultants') ); ?>">Consultants</a></li>
-							</ul>
-						</div>
+
+						<h4>Get help</h4>
+						<ul>
+							<li><a href="<?php echo esc_url( site_url('support') ); ?>">Support</a></li>
+							<li><a href="http://docs.affiliatewp.com/" target="_blank">Documentation</a></li>
+							<li><a href="<?php echo esc_url( site_url('consultants') ); ?>">Consultants</a></li>
+						</ul>
+
 					</div>
 
-					<div class="col-xs-12 col-sm-3">
-						<div class="wrap">
-							<h4>Our Products</h4>
-							<ul>
-								<li><a href="https://restrictcontentpro.com/?ref=4570" target="_blank">Restrict Content Pro</a></li>
-								<li><a href="https://easydigitaldownloads.com/?ref=4657" target="_blank">Easy Digital Downloads</a></li>
-							</ul>
-						</div>
+					<div class="col-xs-12 col-sm-3 meet-the-family">
+
+						<h4>Meet the family</h4>
+						<ul>
+
+							<li>
+								<a href="https://easydigitaldownloads.com/?ref=4657" target="_blank">
+									<img src="<?php echo get_stylesheet_directory_uri() . '/images/meet-easy-digital-downloads.png'; ?>" alt="" />
+									<span>Easy Digital Downloads</span>
+								</a>
+
+							</li>
+
+							<li>
+								<a href="https://restrictcontentpro.com/?ref=4570" target="_blank">
+									<img src="<?php echo get_stylesheet_directory_uri() . '/images/meet-restrict-content-pro.png'; ?>" alt="" />
+									<span>Restrict Content Pro</span>
+								</a>
+							</li>
+						</ul>
+
 					</div>
 
 				</div>
