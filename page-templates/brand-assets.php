52db89950abbd91b7ae90e5e920329b741a756fa
Andrew Munro <andrew@sumobi.com>
2016-08-27 15:22:27 +1200

    brand assets

--- page-templates/brand-assets.php
+++ page-templates/brand-assets.php
@@ -5,13 +5,22 @@
 
 get_header(); ?>
 
-<?php themedd_post_header( array( 'title' => 'Brand Assets', 'subtitle' => 'Thanks for your interest in AffiliateWP!' ) ); ?>
+<?php
 
-<section class="container-fluid">
+themedd_post_header(
+	array(
+		'title' => 'Brand Assets',
+		'subtitle' => 'Thanks for your interest in AffiliateWP!',
+		'classes' => array( 'highlight' )
+	)
+);
+?>
+
+<section class="container-fluid pv-xs-2 pv-sm-4 brand-assets">
 	<div class="row center-xs aligncenter">
-		<div class="col-xs-12 col-sm-8 col-lg-5">
+		<div class="col-xs-12 col-sm-8">
 			<p class="mb-xs-4">Below are a few guidelines for using AffiliateWP’s brand resources, please take a moment to familiarize yourself with them. You can download individual assets in each section, or you can download everything all at once below.</p>
-			<a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/all.zip'; ?>" class="button center-xs">Download all assets</a>
+			<a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/all.zip'; ?>" class="button center-xs large">Download all assets</a>
 		</div>
 	</div>
 </section>
@@ -21,15 +30,15 @@ get_header(); ?>
  * Colors
  */
 ?>
-<section class="container-fluid pv-xs-2 pv-md-4 bg colors">
+<section class="container-fluid pv-xs-2 pv-md-4 colors brand-assets">
 
 	<div class="row center-xs">
 
 		<div class="col-xs-12 col-md-8">
 
 
-			<h1>Colors</h1>
-			<p>These are AffiliateWP's brand colors. Copy the HEX codes below.</p>
+			<h1 class="aligncenter">Colors</h1>
+			<p class="aligncenter">These are AffiliateWP's brand colors. Copy the HEX codes below.</p>
 
 		    <div class="row">
 
@@ -73,7 +82,7 @@ get_header(); ?>
  * Logo guidelines
  */
 ?>
-<section class="container-fluid highlight pv-xs-2 pv-md-4 bg colors">
+<section class="container-fluid pv-xs-2 pv-md-4 bg logos aligncenter brand-assets">
 
 	<div class="row center-xs">
 
@@ -84,28 +93,109 @@ get_header(); ?>
 
 		    <div class="row">
 
-		        <div class="col-xs-12 col-sm-6">
-					<div class="logo-1">
-						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/affiliatewp-1.png'; ?>">
+		        <div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-1 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.svg'; ?>">
 					</div>
-					<h4>1. Logo on #FFFFFF</h4>
-					<p>Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.ai'; ?>">.AI</a>,
-						<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.eps'; ?>">.EPS</a> or
-						<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.png'; ?>">.PNG</a>
+					<p>
+						1. Logo on #FFFFFF<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/1/affiliatewp-1.png'; ?>">PNG</a>
 					</p>
 		        </div>
 
-				<div class="col-xs-12 col-sm-6">
-					<div class="logo-2">
-						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/affiliatewp-1.png'; ?>">
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-2 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.svg'; ?>">
 					</div>
-					<h4>2. Logo on #2D2D2D</h4>
-					<p>Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.ai'; ?>">.AI</a>,
-						<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.eps'; ?>">.EPS</a> or
-						<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.png'; ?>">.PNG</a>
+					<p>
+						2. Logo on #2D2D2D<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/2/affiliatewp-2.png'; ?>">PNG</a>
 					</p>
 		        </div>
 
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-3 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/3/affiliatewp-3.svg'; ?>">
+					</div>
+					<p>
+						3. White logo on #E34F43<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/3/affiliatewp-3.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/3/affiliatewp-3.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/3/affiliatewp-3.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/3/affiliatewp-3.png'; ?>">PNG</a>
+					</p>
+		        </div>
+
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-4 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/4/affiliatewp-4.svg'; ?>">
+					</div>
+					<p>
+						4. White logo on #ED6D62<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/4/affiliatewp-4.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/4/affiliatewp-4.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/4/affiliatewp-4.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/horizontal/4/affiliatewp-4.png'; ?>">PNG</a>
+					</p>
+		        </div>
+
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-5 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/5/affiliatewp-5.svg'; ?>">
+					</div>
+					<p>
+						5. Logo on #FFFFFF<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/5/affiliatewp-5.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/5/affiliatewp-5.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/5/affiliatewp-5.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/5/affiliatewp-5.png'; ?>">PNG</a>
+					</p>
+		        </div>
+
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-6 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/6/affiliatewp-6.svg'; ?>">
+					</div>
+					<p>
+						6. Logo on #2D2D2D<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/6/affiliatewp-6.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/6/affiliatewp-6.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/6/affiliatewp-6.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/6/affiliatewp-6.png'; ?>">PNG</a>
+					</p>
+		        </div>
+
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-7 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/7/affiliatewp-7.svg'; ?>">
+					</div>
+					<p>
+						7. White logo on #E34F43<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/7/affiliatewp-7.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/7/affiliatewp-7.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/7/affiliatewp-7.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/7/affiliatewp-7.png'; ?>">PNG</a>
+					</p>
+		        </div>
+
+				<div class="col-xs-12 col-sm-6 mb-xs-2">
+					<div class="logo-8 mb-xs-1">
+						<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/8/affiliatewp-8.svg'; ?>">
+					</div>
+					<p>
+						8. White logo on #ED6D62<br/>
+						Download <a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/8/affiliatewp-8.ai'; ?>">AI</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/8/affiliatewp-8.eps'; ?>">EPS</a>,
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/8/affiliatewp-8.svg'; ?>">SVG</a> or
+							<a target="_blank" href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos/vertical/8/affiliatewp-8.png'; ?>">PNG</a>
+					</p>
+		        </div>
 
 		    </div>
 
@@ -113,6 +203,265 @@ get_header(); ?>
 
 	</div>
 
+	<div class="wrapper">
+		<a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/logos.zip'; ?>" target="_blank" class="button large">Download logos</a>
+	</div>
+
 </section>
+
+
+<?php
+/**
+ * A little room to breathe
+ */
+?>
+<section class="container-fluid pv-xs-2 pv-md-4 aligncenter brand-assets">
+
+	<div class="row center-xs">
+		<div class="col-xs-12 col-sm-6">
+			<h1>A little room to breathe</h1>
+			<p>There should always be at least 1 unit of space around the entire logo. A unit is defined by the largest circle's diameter that appears in the logo mark.</p>
+		</div>
+	</div>
+
+	<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/logo-guidelines.png'; ?>">
+
+</section>
+
+<?php
+/**
+ * It's AffiliateWP
+ */
+?>
+<section class="container-fluid pv-xs-2 pv-md-8 aligncenter brand-assets">
+
+	<div class="row center-xs">
+		<div class="col-xs-12 col-sm-6">
+			<h1>It's AffiliateWP</h1>
+			<p>AffiliateWP is one word, just like the above. Capitalize the A, W, and P.</p>
+		</div>
+	</div>
+
+	<div class="wrapper">
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-6 col-lg-3 incorrect mb-xs-2 mb-sm-0">
+				<h2>Affiliate WP</h2>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Not like this.</span>
+				</p>
+
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-3 incorrect mb-xs-2 mb-sm-0">
+				<h2>affiliateWP</h2>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Not like this.</span>
+				</p>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-3 incorrect mb-xs-2 mb-sm-0">
+				<h2>affiliatewp</h2>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Not like this.</span>
+				</p>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-3 incorrect mb-xs-2 mb-sm-0">
+
+				<h2>AFFILIATEWP</h2>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Not like this.</span>
+				</p>
+
+		</div>
+
+	</div>
+
+
+
+</section>
+
+
+
+<?php
+/**
+ * Say hi to Alf!
+ */
+?>
+<section class="container-fluid pv-xs-2 pv-md-4 aligncenter mascots">
+
+	<div class="row center-xs mb-xs-2">
+		<div class="col-xs-12 col-sm-6">
+			<h1>Say hi to Alf!</h1>
+			<p>Alf is our mascot. We don’t use him as part of the AffiliateWP logo. Feel free to use any of the images below in combination with any of the background colors shown.</p>
+		</div>
+	</div>
+
+	<div class="wrapper">
+
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-1">
+
+				<div class="mb-xs-2">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf.png'; ?>">
+				</div>
+
+				<h3>Standard Alf</h3>
+				<p>Download <a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/alf/alf.png'; ?>" target="_blank">.PNG</a><br>
+				Background: #E34F43
+				</p>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-2">
+
+
+				<div class="mb-xs-2">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf-winking.png'; ?>">
+				</div>
+
+				<h3>Winking Alf</h3>
+
+				<p>Download <a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/alf/alf-winking.png'; ?>" target="_blank">.PNG</a><br>
+				Background: #ED6D62
+				</p>
+
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-3">
+
+				<div class="mb-xs-2">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf-smiling.png'; ?>">
+				</div>
+
+				<h3>Smiling Alf</h3>
+
+				<p>Download <a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/alf/alf-smiling.png'; ?>" target="_blank">.PNG</a><br>
+				Background: #FFFFFF
+				</p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
+
+
+<?php
+/**
+ * Please don't hurt him
+ */
+?>
+<section class="container-fluid pv-xs-2 pv-md-4 aligncenter mascots incorrect-mascots">
+
+	<div class="row center-xs mb-xs-2">
+		<div class="col-xs-12 col-sm-6">
+			<h1>Please don't hurt Alf</h1>
+			<p>Alf should always peek out of the space he's allocated as shown above, and shouldn't just float in empty space with half his body chopped off — that will surely make him angry!</p>
+		</div>
+	</div>
+
+	<div class="wrapper">
+
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-1 incorrect mb-xs-2 mb-sm-0">
+
+				<div class="mb-xs-2 mascot-image">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf.png'; ?>">
+				</div>
+
+
+				<h3>Ouch!</h3>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Please don't do this.</span>
+				</p>
+
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-2 incorrect mb-xs-2 mb-sm-0">
+
+
+				<div class="mb-xs-2 mascot-image">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf-winking.png'; ?>">
+				</div>
+				<h3>Ow!</h3>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Please don't do this.</span>
+				</p>
+
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 mascot-3 incorrect mb-xs-2 mb-sm-0">
+
+				<div class="mb-xs-2 mascot-image">
+					<img alt="AffiliateWP" src="<?php echo get_stylesheet_directory_uri() . '/images/brand-assets/alf-smiling.png'; ?>">
+				</div>
+
+				<h3>Dang it!</h3>
+				<p>
+					<svg width="48px" height="48px">
+						<use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-';?>cancel"></use>
+					</svg>
+					<span>Please don't do this.</span>
+				</p>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+</section>
+
+<section class="container-fluid aligncenter brand-assets pv-xs-2 pv-md-4 ">
+	<div class="wrapper">
+		<a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/alf.zip'; ?>" target="_blank" class="button large">Download Alf</a>
+	</div>
+</section>
+
+
+<?php
+/**
+ * Screenshots
+ */
+?>
+<section class="container-fluid pv-xs-2 pv-md-4 aligncenter mascots incorrect-mascots">
+
+	<div class="row center-xs">
+		<div class="col-xs-12 col-sm-6">
+			<h1>Screenshots</h1>
+			<p class="mb-xs-4">We currently have 35 screenshots of the Affiliate Dashboard and WordPress admin area. You can either <a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/screenshots.zip'; ?>">download this zip file</a> or <a href="<?php echo site_url( 'screenshots' );?>">view and download</a> the screenshots individually.</p>
+			<a href="<?php echo get_stylesheet_directory_uri() . '/affiliatewp-brand-assets/screenshots.zip'; ?>" target="_blank" class="button large">Download screenshots</a>
+		</div>
+	</div>
+
+
+
+</section>
+
 
 <?php get_footer();
