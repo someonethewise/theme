4c78040f742ac5cb463444fb8cc8f294218ddd0d
Andrew Munro <andrew@sumobi.com>
2016-08-29 12:53:21 +1200

    add-on page template improvements

--- page-templates/add-ons.php
+++ page-templates/add-ons.php
@@ -7,61 +7,64 @@ get_header(); ?>
 
 <?php themedd_post_header( array( 'title' => 'Make AffiliateWP more powerful with add-ons', 'subtitle' => 'Extend AffiliateWP’s functionality with official and 3rd-party add-ons' ) ); ?>
 
-<?php
+<section class="container-fluid highlight add-ons pv-xs-1">
+	<div class="wrapper">
 
-$pro_add_on_page = get_page_by_title( 'Pro add-ons' );
-$pro_add_on_page = $pro_add_on_page ? $pro_add_on_page->ID : '';
-
-$official_free_add_on_page = get_page_by_title( 'Official free add-ons' );
-$official_free_add_on_page = $official_free_add_on_page ? $official_free_add_on_page->ID : '';
-
-$third_party_add_on_page = get_page_by_title( '3rd-party add-ons' );
-$third_party_add_on_page = $third_party_add_on_page ? $third_party_add_on_page->ID : '';
-
-$args = array(
-    'include' => array( $pro_add_on_page, $official_free_add_on_page, $third_party_add_on_page ),
-    'sort_column' => 'menu_order',
-);
-
-$pages = get_pages( $args );
-
-$count = 0;
-
-if ( $pages ) : ?>
-
-<section class="container-fluid highlight add-ons pv-xs-4">
-	<div class="container-fluid wrapper">
 		<div class="row">
-			<?php foreach ( $pages as $page ) : ?>
-				<div class="col-xs-12 col-sm-6 col-md-4 grid-item">
-					<div class="grid-item-inner">
-						<?php if ( themedd_post_thumbnail() ) : ?>
-						<div class="grid-item-image">
-							<?php themedd_post_thumbnail(); ?>
+
+			<?php
+				$categories = get_terms( 'download_category', array(
+				    'orderby'    => 'name',
+					'order'      => 'DESC',
+				    'hide_empty' => 0,
+				) );
+			?>
+
+			<?php if ( $categories ) : ?>
+
+				<?php foreach ( $categories as $category ) : ?>
+
+					<?php if ( themedd_post_thumbnail() ) : ?>
+					<div class="grid-item-image">
+						<?php themedd_post_thumbnail(); ?>
+					</div>
+					<?php endif; ?>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 grid-item">
+
+						<div class="grid-item-inner">
+
+							<?php if ( themedd_post_thumbnail() ) : ?>
+							<div class="grid-item-image">
+								<?php themedd_post_thumbnail(); ?>
+							</div>
+							<?php endif; ?>
+
+							<div>
+								<h3>
+									<a href="<?php echo get_the_permalink() . $category->slug; ?>">
+										<?php echo $category->name; ?>
+									</a>
+								</h3>
+
+								<p><?php echo $category->description; ?></p>
+							</div>
+
+							<footer>
+								<a href="<?php echo get_the_permalink() . $category->slug; ?>">View add-ons</a>
+							</footer>
+
 						</div>
-						<?php endif; ?>
-
-						<div>
-							<h3>
-								<a href="<?php echo get_the_permalink( $page->ID ); ?>">
-									<?php echo get_the_title( $page->ID ); ?>
-								</a>
-							</h3>
-
-							<p><?php echo $page->post_excerpt; ?></p>
-						</div>
-
-						<footer>
-	                        <a href="<?php echo get_the_permalink( $page->ID ); ?>">View add-ons</a>
-	                    </footer>
 
 					</div>
-				</div>
-			<?php endforeach; ?>
+
+				<?php endforeach; ?>
+
+			<?php endif; ?>
+
 		</div>
+
 	</div>
 </section>
-
-<?php endif; ?>
 
 <?php get_footer();
