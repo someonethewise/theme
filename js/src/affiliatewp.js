02986ef46fcc2f6f71cbbb213637a6d3afbcc752
Andrew Munro <andrew@sumobi.com>
2016-10-24 18:49:32 +1300

    Simplify JS

--- js/src/affiliatewp.js
+++ js/src/affiliatewp.js
@@ -1,24 +1,5 @@
 jQuery(document).ready(function($) {
 
-	// link the last tab to the affiliate area page
-	if ( $('#tabs').length ) {
-		jQuery('#tabs').tabs({
-
-			beforeActivate: function(event, ui) {
-
-				if ( ui.newTab.data("link") === 'affiliate-area' ) {
-
-					event.preventDefault();
-					var url = $('.follow-link a').attr('href');
-					location.href = url;
-					return false;
-
-				}
-
-			}
-		});
-	}
-
 	// add loaded class
 	jQuery('body').addClass('loaded');
 
