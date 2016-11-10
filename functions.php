e5778024af95ef0e940ffeec5ab885d8e40fd963
Andrew Munro <andrew@sumobi.com>
2016-11-09 21:12:04 -0800

    CSS

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'AFFWP_THEME_INCLUDES_DIR' ) ) {
 }
 
 if ( ! defined( 'AFFWP_THEME_VERSION' ) ) {
-	define( 'AFFWP_THEME_VERSION', '1.1.6' );
+	define( 'AFFWP_THEME_VERSION', '1.1.7' );
 }
 
 if ( ! defined( 'THEMEDD_VERSION' ) ) {
